cd2688a6b7bc60090603b6e5cf13cb303a70a7c2
Paul Haddad <paulh16@gmail.com>
2016-01-05 20:34:53 -0600

    Refactor greeting method

--- exercise_1/say_hello.rb
+++ exercise_1/say_hello.rb
@@ -1,13 +1,11 @@
-#Say Hello
-
-def greeting(name)
-  "Hello, #{name}, nice to meet you!"
-end
-
 def main
   print "What is your name? "
   name = gets.chomp
   puts greeting(name)
+end
+
+def greeting(name)
+  "Hello, #{name}, nice to meet you!"
 end
 
 main
