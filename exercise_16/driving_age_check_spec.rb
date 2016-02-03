4962e5b95ae59d853464b50a9a939b060d725c75
Paul Haddad <paulh16@gmail.com>
2016-02-02 18:34:48 -0600

    Exercise 16 - Iteration 1

---
+++ exercise_16/driving_age_check_spec.rb
@@ -0,0 +1,32 @@
+require "./driving_age_check"
+
+describe DrivingAgeCheck do
+  describe "#legal_driver?" do
+    it "returns true if the driver is older than the legal driving age" do
+      age = 20
+      age_checker = create_age_checker(age: age)
+
+      expect(age_checker.legal_driver?).to be_truthy
+    end
+  end
+
+    it "returns true if the driver is the legal driving age" do
+      age = 16
+      age_checker = create_age_checker(age: age)
+
+      expect(age_checker.legal_driver?).to be_truthy
+    end
+
+    it "returns true if the driver is younger than the legal driving age" do
+      age = 15
+      age_checker = create_age_checker(age: age)
+
+      expect(age_checker.legal_driver?).to be_falsey
+    end
+
+  private
+
+  def create_age_checker(age:)
+    DrivingAgeCheck.new(age: age)
+  end
+end
